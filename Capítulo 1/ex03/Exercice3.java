4b6ae9926c15224b01087c5947bded82f9365b35
Eduardo499 <dudu.araujo.ferreira@outlook.com>
2025-01-19 13:15:37 -0300

    ex03

---
+++ Capítulo 1/ex03/Exercice3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercice3 {
+    public static void main(String[] args) {
+        int c, thisIsAVariable, q76354, number;
+        System.out.print("Enter a integer: ");
+        Scanner input = new Scanner(System.in);
+        number = input.nextInt();
+        System.out.println("This is a Java program");
+        System.out.printf("%s%n%s%n", "This is a Java", "Program");
+        if (number != 7) {
+            System.out.println("The variable number is not equal to 7");
+        }
+    }
+}
