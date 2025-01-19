9cb669c8f8e87b007503155626ffdb4a56958661
Eduardo499 <dudu.araujo.ferreira@outlook.com>
2025-01-19 14:07:05 -0300

    ex14, ex15

---
+++ Capítulo 1/ex15/Arithmetic.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        int number1, number2, add, sub, mul;
+        float div;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a integer: ");
+        number1 = input.nextInt();
+
+        System.out.print("Enter other integer: ");
+        number2 = input.nextInt();
+
+        add = number1 + number2;
+        sub = number1 - number2;
+        mul = number1 * number2;
+        div = (float) number1 / number2;
+
+        System.out.printf("%d + %d = %d%n", number1, number2, add);
+        System.out.printf("%d - %d = %d%n", number1, number2, sub);
+        System.out.printf("%d X %d = %d%n", number1, number2, mul);
+        System.out.printf("%d / %d = %.2f%n", number1, number2, div);
+    }
+}
