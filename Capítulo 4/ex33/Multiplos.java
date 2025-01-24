891c759645c71f90a8f5c6c2255e10aa1b6e6358
Eduardo de Araújo Ferreira <64329536+Eduardo499@users.noreply.github.com>
2025-01-24 12:03:30 -0300

    ex33

---
+++ Capítulo 4/ex33/Multiplos.java
@@ -0,0 +1,13 @@
+package ex33;
+
+public class Multiplos {
+    public static void main(String[] args) {
+        int multiplo = 2;
+
+        while (true) {
+            System.out.println(multiplo);
+            multiplo *= 2;
+
+        }
+    }
+}
