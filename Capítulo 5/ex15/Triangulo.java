4b28c8abb01c4113f4fb28edfce56249285dc5f8
Eduardo499 <dudu.araujo.ferreira@outlook.com>
2025-01-25 15:29:56 -0300

    ex15

---
+++ Capítulo 5/ex15/Triangulo.java
@@ -0,0 +1,45 @@
+package ex15;
+
+public class Triangulo {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 10; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 10; i >= 1; i--) {
+            for (int j = 1; j <= 10 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
