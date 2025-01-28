9cc0cc87db79754d95e900b2ecb4ad27f55ce93a
Eduardo499 <dudu.araujo.ferreira@outlook.com>
2025-01-28 14:44:16 -0300

    ex18

---
+++ Capítulo 6/ex18/Square.java
@@ -0,0 +1,16 @@
+package ex18;
+
+public class Square {
+    public static void main(String[] args) {
+        squareOfAsteristks(20);
+    }
+    public static void squareOfAsteristks(int side) {
+        for (int i = 1; i <= side; i++) {
+            for (int j = 1; j <= side; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
