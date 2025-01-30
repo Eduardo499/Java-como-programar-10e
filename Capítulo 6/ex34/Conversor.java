a8a7fb8506bc59f2ab5fef3816a7192cd8d17036
Eduardo499 <dudu.araujo.ferreira@outlook.com>
2025-01-30 14:06:54 -0300

    ex34

---
+++ Capítulo 6/ex34/Conversor.java
@@ -0,0 +1,10 @@
+package ex34;
+
+public class Conversor {
+    public static void main(String[] args) {
+        System.out.printf("%-10s %-10s %-10s %-10s%n", "Decimal", "Binary", "Octal", "Hexadecimal");
+        for (int i = 1; i <= 256; i++) {
+            System.out.printf("%-10d %-10s %-10s %-10s%n", i, Integer.toBinaryString(i), Integer.toOctalString(i), Integer.toHexString(i).toUpperCase());
+        }
+    }
+}
